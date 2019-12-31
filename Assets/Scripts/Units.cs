9f0a6c5821d80975eb1fb75e80b587426d5f2b67
tanhahaha123 <tanhahaha123.mail@gmail.com>
2019-12-31 13:38:31 +0700

    Updated Script
    update cac ham chien dau trog units

--- Assets/Scripts/Units.cs
+++ Assets/Scripts/Units.cs
@@ -2,144 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
 
 public class Units : MonoBehaviour
 {
-    [SerializeField] int health;
-    [SerializeField] int attack;
-    [SerializeField] int defend;
-    [SerializeField] int speed;
-    [SerializeField] int cost;
+    [SerializeField] float health;
+    [SerializeField] public float attack;
+    [SerializeField] public float defend;
+    [SerializeField] public float amount;
+    [SerializeField] float speed;
+    [SerializeField] float cost;
+
+    [SerializeField] Image HPBar;
+    [SerializeField] public float CDTime = 0f; //giam het amount trong CDTime
+    [SerializeField] float DPSFromEnemy = new float();
+    float TimeStop;// dung de dung HPBar giam khi xong chien dau
+    GameObject EnemyUnit;
     //player hay computer
 
-    class TwoValue
+    void OnTriggerEnter(Collider Other)
     {
-        public Building fatherBuilding;
-        public float lenghFromStart;
-        public TwoValue(Building b, float l)
+        EnemyUnit=Other.gameObject;
+        Units UnitEnemy = Other.gameObject.GetComponent<Units>();
+        DPSFromEnemy=CaculateDPS(this, UnitEnemy);
+        CDTime = amount / DPSFromEnemy;
+       
+        print("tiep xuc");
+    }
+    void OnTriggerExit(Collider Other)
+    {
+       if(Other.gameObject==EnemyUnit)
         {
-            fatherBuilding = b;
-            lenghFromStart = l;
+            EnemyUnit = null;
         }
     }
-    void run()
+
+    public float CaculateDPS(Units unit1, Units unit2)
     {
-        List<Building> buildings = new List<Building>();
-
-        Building A = new Building(), B = new Building(), C = new Building(), D = new Building(), E = new Building(), F = new Building();
-
-        A.name = "A";
-        A.ways.Add(B, 0.5f);
-        A.ways.Add(C, 1f);
-        A.ways.Add(D, 6f);
-
-        B.name = "B";
-        B.ways.Add(A, 0.5f);
-        B.ways.Add(F, 10f);
-        B.ways.Add(D, 1f);
-
-
-        C.name = "C";
-        C.ways.Add(A, 1f);
-        C.ways.Add(E, 5f);
-
-        D.name = "D";
-        D.ways.Add(A, 6f);
-        D.ways.Add(E, 1f);
-        D.ways.Add(B, 1f);
-
-        E.name = "E";
-        E.ways.Add(C, 5f);
-        E.ways.Add(D, 1f);
-
-        F.name = "F";
-        F.ways.Add(B, 10f);
-
-
-        buildings.Add(A);
-        buildings.Add(B);
-        buildings.Add(C);
-        buildings.Add(D);
-        buildings.Add(E);
-
-        var start = A;
-        var end = E;
-        //           index ,  cha
-        Dictionary<Building, TwoValue> tree = new Dictionary<Building, TwoValue>();
-        Dictionary<Building, TwoValue> temp = new Dictionary<Building, TwoValue>();
-        var index = start;
-        tree.Add(A, new TwoValue(null, 0));
-        //Dictionary<Building, bool> checkList = new Dictionary<Building, bool>();
-
-
-
-        while (!tree.ContainsKey(end))
+        //DPS: amount giam trong 1 giay
+       
+        float DPS = new float();
+        DPS = (unit2.attack * unit2.amount - unit1.defend * unit1.amount);
+        if (DPS == 0)
         {
-            foreach (var element in index.ways)
-            {
-                if (!temp.ContainsKey(element.Key) && !tree.ContainsKey(element.Key))
-                {
-                    temp.Add(element.Key, new TwoValue(index, tree[index].lenghFromStart + element.Value));
-                }
-            }
-
-            var currentMin = temp.Aggregate((l, r) => l.Value.lenghFromStart < r.Value.lenghFromStart ? l : r);
-            //checkList[currentMin.Key] = true;
-
-            if (tree.Count >= buildings.Count || temp.Count == 0)
-                break;
-
-
-            foreach (var element in temp)
-            {
-                if (element.Key.ways.ContainsKey(index))
-                {
-                    if (tree[index].lenghFromStart + element.Key.ways[index] < element.Value.lenghFromStart)
-                    {
-                        element.Value.fatherBuilding = index;
-                        element.Value.lenghFromStart = tree[index].lenghFromStart + element.Key.ways[index];
-                    }
-
-                }
-            }
-            temp.Remove(currentMin.Key);
-
-            try
-            {
-                if (currentMin.Value.lenghFromStart > tree[index].lenghFromStart + index.ways[currentMin.Key])
-                    tree.Add(currentMin.Key, new TwoValue(index, tree[index].lenghFromStart + index.ways[currentMin.Key]));
-                else
-                    tree.Add(currentMin.Key, new TwoValue(currentMin.Value.fatherBuilding, currentMin.Value.lenghFromStart));
-            }
-            catch
-            {
-                tree.Add(currentMin.Key, new TwoValue(currentMin.Value.fatherBuilding, currentMin.Value.lenghFromStart));
-            }
-            index = currentMin.Key;
+            DPS = 1f;
         }
-
-        List<Building> result = new List<Building>();
-        if (tree.ContainsKey(end))
+        return DPS;
+    }
+    //BUG: ko chay cung luc khi HPBar giam
+    //0.031 const de amount va HPbar chay cung luc
+    IEnumerator amountDE()
+    {
+        amount -= DPSFromEnemy*0.031f; 
+        yield return new WaitForSeconds(1f);
+    }
+    void Start()
+    {
+       
+    }
+    void Update()
+    {
+        HPBar.fillAmount -= (1f / CDTime * Time.deltaTime)*TimeStop;
+        StartCoroutine(amountDE());
+        if (amount <= 0)
         {
-            var i = end;
-            while (i != null)
-            {
-
-                result.Add(i);
-                i = tree[i].fatherBuilding;
-            }
-            result.Reverse();
-
-
-            foreach (var e in result)
-            {
-                print(e.name);
-            }
-            print("");
+            //UnitEnemy.CDTime = 0;
+            //UnitEnemy.DPSFromEnemy = 0f;
+            Destroy(gameObject);
+        }
+        if(EnemyUnit==null)
+        {
+            DPSFromEnemy = 0f;
+            TimeStop = 0;
         }
         else
         {
-            print("khong tim thay");
+            TimeStop = 1;
         }
-    }
+    } 
 }
